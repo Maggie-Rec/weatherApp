02308bc4d359c8b135958a6e76c9bfae022256ba
maggie-Rec <marzena.gromo@gmail.com>
2022-04-23 16:45:07 +0100

    added relevant icon

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ function displayWeather(response) {
   pressure.innerHTML = response.data.main.pressure;
   let cityName = document.querySelector("#change-city");
   cityName.innerHTML = response.data.name;
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function showCity(event) {
